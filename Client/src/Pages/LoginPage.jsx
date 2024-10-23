8a8aa18f068f853a3eb2afc228db010848466dd9
ANDRESGOM77 <141623667+ANDRESGOM77@users.noreply.github.com>
2024-10-23 21:51:06 +0200

    Log in UI

--- Client/src/Pages/LoginPage.jsx
+++ Client/src/Pages/LoginPage.jsx
@@ -1,9 +1,29 @@
-
+import { useState } from "react";
+import { Lock, Mail, User } from "lucide-react";
+import { motion } from "framer-motion";
+import { Link } from "react-router-dom";
 
 const LoginPage = () => {
-  return (
-    <div>LoginPage</div>
-  )
-}
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-export default LoginPage
+  const handleLogin = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <motion.div
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.5 }}
+      className="max-w-md w-full bg-gray-800 bg-opacity-50 backdrop-filter backdrop-blur-xl rounded-2xl shadow-xl overflow-hidden"
+    >
+      <div className="p-8">
+        <h2 className=" text-3xl font-bold mb-6 text-center bg-gradient-to-r from-green-400 to-emerald-500 text-transparent bg-clip-text">
+          Welcome Back
+        </h2>
+      </div>
+    </motion.div>
+  );
+};
+
+export default LoginPage;
